bab18302db9098fc2c0ed6ea75750cfcd4ac8ab4
Khushi Yadav <kky957242@gmail.com>
2025-05-22 18:44:43 +0530

    Create Day-1.sql

---
+++ Day-1.sql
@@ -0,0 +1,46 @@
+SQL (pronounced as "ess-que-el" or "sequel") stands for Structured Query Language. It is the standard language used to manage and manipulate databases.
+  SQL was developed in the 1970s by IBM researchers and became a standard by ANSI (American National Standards Institute).
+
+  
+🔹 Purpose of SQL:
+- To store, retrieve, update, and delete data in a database.
+- To create and modify the structure of database systems (like tables).
+- To control access to the data (security, permissions).
+*********************************************************************************
+
+  
+🟢 Why Learn SQL?
+💼 Used in data analysis, software development, web apps, and backend services.
+
+📊 Essential for data analysts, data scientists, and backend developers.
+
+🔍 Helps you work with large amounts of data stored in relational databases.
+**********************************************************************************
+
+  
+ Popular Databases that use SQL:
+MySQL
+
+PostgreSQL
+
+Oracle
+
+Microsoft SQL Server
+
+SQLite
+*****************************************************************************************
+
+  
+ Basic SQL Syntax:
+SQL statements are not case-sensitive, but keywords are often written in UPPERCASE for clarity.
+  for example- ( SELECT name, age FROM students;  )
+  
+here, Explanation:
+SELECT: Command to retrieve data.
+name, age: Columns to fetch.
+FROM students: Table name from where to fetch data
+************************************************************
+
+
+
+
