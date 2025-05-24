3c0c0b430d28ee23d5f4cf49639d26fdadf81383
Khushi Yadav <kky957242@gmail.com>
2025-05-24 19:30:45 +0530

    Create Day-5.sql

---
+++ Day-5.sql
@@ -0,0 +1,15 @@
+The ORDER BY keyword sorts the records in ascending order by default. To sort the records in descending order, use the DESC keyword.
+
+Example
+Sort the products from highest to lowest price:
+
+SELECT * FROM Products
+ORDER BY Price DESC;
+Order Alphabetically
+For string values the ORDER BY keyword will order alphabetically:
+
+Example
+Sort the products alphabetically by ProductName:
+
+SELECT * FROM Products
+ORDER BY ProductName;
