b1e1f71fac52ece26de92889e580bd58b53fb8d0
Khushi Yadav <kky957242@gmail.com>
2025-05-24 19:28:47 +0530

    Create Day-4.py

---
+++ Day-4.py
@@ -0,0 +1,11 @@
+The ORDER BY keyword is used to sort the result-set in ascending or descending order.
+
+ExampleGet your own SQL Server
+Sort the products by price:
+
+SELECT * FROM Products
+ORDER BY Price;
+Syntax
+SELECT column1, column2, ...
+FROM table_name
+ORDER BY column1, column2, ...
