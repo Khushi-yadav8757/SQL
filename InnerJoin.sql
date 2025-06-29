86345928f8ccebeb38d9fcc13adecb69064e0d16
Khushi Yadav <kky957242@gmail.com>
2025-06-29 09:03:37 +0530

    Create InnerJoin.sql

---
+++ InnerJoin.sql
@@ -0,0 +1,8 @@
+Q6. Write a query using INNER JOIN to display employee name and their department name.
+ans:--
+  
+SELECT e.name, d.department_name 
+FROM employees e 
+INNER JOIN departments d 
+ON e.department_id = d.id;
+
