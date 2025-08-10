0fb37679ec65905bb92bf91c0ea0a6ec285d81e2
Khushi Yadav <kky957242@gmail.com>
2025-08-10 19:43:31 +0530

    Create Basicc.sql

---
+++ Basicc.sql
@@ -0,0 +1,24 @@
+#IN
+SELECT * FROM employees WHERE position IN ('Manager', 'Developer');
+
+#LIKE
+SELECT * FROM employees WHERE name LIKE 'J%';
+
+IS NULL
+SELECT * FROM employees WHERE salary IS NULL;
+
+UPDATE
+UPDATE employees SET salary = 65000 WHERE id = 1;
+
+DELETE
+DELETE FROM employees WHERE id = 1;
+
+Aggregate functions
+SELECT COUNT(*), AVG(salary), MAX(salary), MIN(salary) FROM employees;
+--------------------------------------------------------------------
+GROUP BY
+SELECT position, AVG(salary) 
+FROM employees 
+GROUP BY position;
+
+
