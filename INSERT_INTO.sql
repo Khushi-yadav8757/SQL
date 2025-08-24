fd9d8c125961d1a2dbd81416d96073d797ef68f7
Khushi Yadav <kky957242@gmail.com>
2025-08-24 22:00:35 +0530

    Create INSERT_INTO.sql

---
+++ INSERT_INTO.sql
@@ -0,0 +1,8 @@
+The INSERT INTO statement is used to insert new records in a table.
+  syntax:
+INSERT INTO table_name (column1, column2, column3, ...)
+VALUES (value1, value2, value3, ...);
+
+example:
+INSERT INTO Customers (CustomerName, City, Country)
+VALUES ('Rishi', 'Delhi', 'INDIA');
