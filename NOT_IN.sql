8bfb9d37760d3d77dedb9ab003727033ea69e33f
Khushi Yadav <kky957242@gmail.com>
2025-08-24 21:54:45 +0530

    Update NOT_IN.sql

--- NOT_IN.sql
+++ NOT_IN.sql
@@ -10,13 +10,18 @@ example1:Select only the customers that are NOT from Spain
 SELECT * FROM Customers
 WHERE NOT Country = 'Spain';
 ---------------------------------------
-example2:Select customers that does not start with the letter 'A'
+example2:Select customers that does not start with the letter 'k'
 
 SELECT * FROM Customers
-WHERE CustomerName NOT LIKE 'A%';
+WHERE CustomerName NOT LIKE 'k%';
 ------------------------------------
-example3:Select customers with a customerID not between 10 and 60:
+example3:Select customers with a customerID not between 10 and 80:
 
 SELECT * FROM Customers
-WHERE CustomerID NOT BETWEEN 10 AND 60;
+WHERE CustomerID NOT BETWEEN 10 AND 80;
 ---------------------------------------------
+example4:Select customers with a CustomerId not greater than 100:
+
+SELECT * FROM Customers
+WHERE NOT CustomerID > 100;
+
