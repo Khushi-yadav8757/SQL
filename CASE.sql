a0dc6020044391b4889932975e3c680f5d8a9841
Khushi Yadav <kky957242@gmail.com>
2025-08-29 23:11:12 +0530

    Create CASE.sql

---
+++ CASE.sql
@@ -0,0 +1,10 @@
+How to use CASE in SQL?
+Question: Categorize employees as 'High' if salary > 70000, otherwise 'Low'.
+Answer:
+
+SELECT Name, Salary,
+       CASE 
+           WHEN Salary > 70000 THEN 'High'
+           ELSE 'Low'
+       END AS SalaryCategory
+FROM Employees;
