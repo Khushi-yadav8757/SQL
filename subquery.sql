611c5b527546f2da527d71b3adc179e8bcf5c8ee
Khushi Yadav <kky957242@gmail.com>
2025-08-29 23:12:11 +0530

    Create subquery.sql

---
+++ subquery.sql
@@ -0,0 +1,7 @@
+What is a subquery?
+Question: Find employees whose salary is greater than the average salary.
+Answer:
+
+SELECT Name, Salary
+FROM Employees
+WHERE Salary > (SELECT AVG(Salary) FROM Employees);
