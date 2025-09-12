c17cf212e4df69a11ae9c85c01b0050d6581c7da
Khushi Yadav <kky957242@gmail.com>
2025-09-12 11:03:05 +0530

    Update subquery.sql

--- subquery.sql
+++ subquery.sql
@@ -1,7 +1,6 @@
 What is a subquery?
 Question: Find employees whose salary is greater than the average salary.
 Answer:
-
 SELECT Name, Salary
 FROM Employees
 WHERE Salary > (SELECT AVG(Salary) FROM Employees);
