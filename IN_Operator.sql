5d20950a4447a8ac584cdd1afc63ef403deb263e
Khushi Yadav <kky957242@gmail.com>
2025-09-20 15:11:25 +0530

    Update IN_Operator.sql

--- IN_Operator.sql
+++ IN_Operator.sql
@@ -1,6 +1,9 @@
 The IN operator allows you to specify multiple values in a WHERE clause.
 The IN operator is a shorthand for multiple OR conditions.
-
-Example:
+  Syntax:
+SELECT column_name(s)
+FROM table_name
+WHERE column_name IN (value1, value2, ...);
+ Example:
 SELECT * FROM Customers
 WHERE Country IN ('Germany', 'France', 'UK');
