09a23578965f36efcff997c181668ad6ce7043b7
Khushi Yadav <kky957242@gmail.com>
2025-10-13 19:02:09 +0530

    Update AvgSal.sql

--- AvgSal.sql
+++ AvgSal.sql
@@ -1,5 +1,4 @@
-Q9. Write a query to display average salary for each department
-ans:-
+--Q9. Write a query to display average salary for each department
 SELECT department_id, AVG(salary) AS avg_salary 
 FROM employees 
 GROUP BY department_id;
