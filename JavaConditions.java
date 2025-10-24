584db1ce4c4186874934308e7a81813647516ded
Khushi Yadav <kky957242@gmail.com>
2025-10-25 00:00:23 +0530

    Add JavaConditions class with main method

---
+++ JavaConditions.java
@@ -0,0 +1,19 @@
+// Source code is decompiled from a .class file using FernFlower decompiler (from Intellij IDEA).
+public class JavaConditions {
+   public JavaConditions() {
+   }
+
+   public static void main(String[] var0) {
+      byte var1 = 16;
+      if (var1 >= 18) {
+         System.out.println("adult: drive,vote ");
+      }
+
+      if (var1 > 16 && var1 < 18) {
+         System.out.println("Teenager");
+      } else {
+         System.out.println("Not Adult ");
+      }
+
+   }
+}
