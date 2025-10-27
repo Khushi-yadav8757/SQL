94cf764a106fcc8a13dfadc40908b1b4063fb0f0
Khushi Yadav <kky957242@gmail.com>
2025-10-27 19:56:38 +0530

    Update Customers.sql

--- Customers.sql
+++ Customers.sql
@@ -1,5 +1,5 @@
-Q. Select all customers from Spain that starts with the letter 'G'
-Ans:-
+--Q. Select all customers from Spain that starts with the letter 'G'
+--Ans:-
 SELECT *
 FROM Customers
 WHERE Country = 'Spain' AND CustomerName LIKE 'G%';
