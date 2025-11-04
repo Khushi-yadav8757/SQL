b08ad14f2e64f9e7d63473ef1a5242cbfaaa4abc
Khushi Yadav <kky957242@gmail.com>
2025-11-04 10:56:36 +0530

    Create OrderDelivery.sql

---
+++ OrderDelivery.sql
@@ -0,0 +1,11 @@
+--Q3. Find the percentage of orders that were delivered late.
+
+--Table: Orders(order_id, delivery_date, expected_date)
+
+--SQL Solution:
+
+SELECT 
+    ROUND(
+        (SUM(CASE WHEN delivery_date > expected_date THEN 1 ELSE 0 END) * 100.0) / COUNT(*), 
+    2) AS late_delivery_percent
+FROM Orders;
