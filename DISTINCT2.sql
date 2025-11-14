87e0fcf62b54b10b0c121be126676491bb820d20
Khushi Yadav <kky957242@gmail.com>
2025-11-14 22:06:02 +0530

    Update and rename DISTINCT.SQL to DISTINCT2.sql

---
+++ DISTINCT2.sql
@@ -0,0 +1,11 @@
+-- DISTINCT -Removve Duplicates Value
+each value appears only one;
+--Syntax
+SELECT DISTINCT
+COLM
+FROM TABLE
+
+ex- Return unique list of all countries
+Select distinct country from Customers;
+
+
