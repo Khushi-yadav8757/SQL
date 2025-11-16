657d0fb4f8c7167148dd8635e3a9f16657da808d
Khushi Yadav <kky957242@gmail.com>
2025-11-16 21:25:26 +0530

    Create AKTUStudents.sql

---
+++ AKTUStudents.sql
@@ -0,0 +1,12 @@
+CREATE TABLE students (
+    id INT,
+    name VARCHAR(50),
+    marks INT
+);
+
+INSERT INTO students VALUES
+(1, 'Amit', 85),
+(2, 'Riya', 92),
+(3, 'Sahil', 76),
+(4, 'Neha', 88),
+(5, 'Raj', 92);
