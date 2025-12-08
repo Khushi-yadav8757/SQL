f640eb65a47dbf8e91f3ff2c9987185661b7bf06
Khushi Yadav <kky957242@gmail.com>
2025-12-08 19:01:38 +0530

    Update TotalPurchase.sql

--- TotalPurchase.sql
+++ TotalPurchase.sql
@@ -1,7 +1,6 @@
 --Q1. Find the top 5 customers by total purchase amount in the last 30 days.
 
 --Table: Orders(order_id, customer_id, order_date, amount)
-
 --SQL Solution:
 SELECT 
     customer_id,
