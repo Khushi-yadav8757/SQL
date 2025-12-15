417f62da3557f3c0f66fcc7ab2eb68727683e158
Khushi Yadav <kky957242@gmail.com>
2025-12-15 21:50:14 +0530

    Create View.sql

---
+++ View.sql
@@ -0,0 +1,17 @@
+--CREATE VIEW 
+CREATE VIEW student_view AS
+SELECT name, age, marks
+FROM students
+WHERE marks > 60;
+
+--RETRIEVE DATA FROM VIEW
+SELECT * FROM student_view;
+
+-- UPDATE VIEW 
+CREATE OR REPLACE VIEW student_view AS
+SELECT name, age
+FROM students;
+
+-- DELETE VIEW
+DROP VIEW student_view;
+
