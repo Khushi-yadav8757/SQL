559f294e0245f0d2d6ac0b1112a9dc2b98376ec1
Khushi Yadav <kky957242@gmail.com>
2025-12-28 20:45:14 +0530

    Update aggregate.sql

--- aggregate.sql
+++ aggregate.sql
@@ -1,7 +1,3 @@
 --What are aggregate functions?
 Functions that perform calculations on multiple rows and return a single value. Examples:
-SUM()
-AVG()
-COUNT()
-MIN()
-MAX()
+SUM(),AVG(),COUNT(),MIN(),MAX()
