9a969f55bea9e3577b8e04e32426f60bad4c445e
Khushi Yadav <kky957242@gmail.com>
2026-01-12 21:44:33 +0530

    Update Count_Employees.sql

--- Count_Employees.sql
+++ Count_Employees.sql
@@ -1,4 +1,4 @@
-Q3. Write a query to count number of employees in each department.
+--Q3. Write a query to count number of employees in each department.
 
 SELECT department_id, COUNT(*) AS employee_count 
 FROM employees 
