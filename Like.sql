e57c2d77eaa5b8857e60f2c29314b55636cb6043
Khushi Yadav <kky957242@gmail.com>
2026-02-02 16:05:03 +0530

    Update Like.sql

--- Like.sql
+++ Like.sql
@@ -1,8 +1,6 @@
-How to use LIKE for pattern matching?
-
-Question: Get all employee names starting with 'J'.
+--How to use LIKE for pattern matching?
+--Question: Get all employee names starting with 'J'.
 Answer:
-
 SELECT Name
 FROM Employees
 WHERE Name LIKE 'J%';
