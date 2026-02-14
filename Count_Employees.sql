b1ca32d79388edfac304d8522951b2a983f855dd
Khushi Yadav <kky957242@gmail.com>
2026-02-14 09:37:19 +0530

    Fix SQL query formatting for employee count

--- Count_Employees.sql
+++ Count_Employees.sql
@@ -1,4 +1,4 @@
 --Q3. Write a query to count number of employees in each department.
-SELECT department_id, COUNT(*) AS employee_count
+SELECT department_id, COUNT(*) AS employee_count 
 FROM employees 
 GROUP BY department_id;
