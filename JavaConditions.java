eaf3f02e8f1246c371519813ee58d552bfb6dab1
Khushi Yadav <kky957242@gmail.com>
2026-02-19 22:25:23 +0530

    Update JavaConditions.java

--- JavaConditions.java
+++ JavaConditions.java
@@ -2,13 +2,11 @@
 public class JavaConditions {
    public JavaConditions() {
    }
-
    public static void main(String[] var0) {
       byte var1 = 16;
       if (var1 >= 18) {
          System.out.println("adult: drive,vote ");
       }
-
       if (var1 > 16 && var1 < 18) {
          System.out.println("Teenager");
       } else {
